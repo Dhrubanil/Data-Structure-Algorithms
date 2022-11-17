1f3dee90676f81adeac0cb28dded16d09b755e20
Dhrubanil <dhrubanil.sikdar@vehere.com>
2022-11-17 08:34:53 +0530

    perfect squares

--- Dynamic_Programming/leetcode/perfect_squares.cpp
+++ Dynamic_Programming/leetcode/perfect_squares.cpp
@@ -16,14 +16,27 @@ int numSquaresByMem(int n, vector<int>&dp) {
     return dp[n];
 }
 
+int numSquaresByTab(int n) {
+    vector<int>dp(n+1,INT_MAX);
+    dp[0] = 0;
+    for(int i=1; i<=n;i++){
+        for(int j=1; j*j<=i;j++){
+            // cout<<i-(j*j)<<endl;
+            dp[i] = min(dp[i],1+dp[i-(j*j)]);
+        }
+    }
+    return dp[n];
+}
+
 int numSquares(int n) {
     
     vector<int> dp(n+1,-1);
-    int ans = numSquaresByMem(n,dp);
+    // int ans = numSquaresByMem(n,dp);
+    int ans = numSquaresByTab(n);
     return ans;
 }
 
 int main(){
-    long long int n= 53;
+    long long int n= 5;
     cout<<numSquares(n)<<endl;
 }
